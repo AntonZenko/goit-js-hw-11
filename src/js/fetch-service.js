3d0421aa75f46bf148660ff52caa7d9e1beb8996
AntonZenko <zenko_anton@ukr.net>
2022-06-22 18:45:36 +0300

    update task-11

---
+++ src/js/fetch-service.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = 'https://pixabay.com/api/';
+const KEY = '28189660-1bf5afecd196ca106a3e3c887';
+
+export default async function getImages(query, page) {
+  const BASE_SEARH_PARAMS = {
+    key: KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 30,
+    page,
+    q: query,
+  };
+
+  const response = await axios.get(API_URL, {
+    params: BASE_SEARH_PARAMS,
+  });
+
+  return response.data;
+}
